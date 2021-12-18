81ed5a7f09365c6de8063d002d4347535e63c584
Kush Mishra <kushmishra05@gmail.com>
2021-12-18 20:22:31 +0530

    Dynamic content using for loop

--- README.md
+++ README.md
@@ -6,9 +6,14 @@ First Project created in Django to learn more and increase the competancy level.
 
 ## For Dynamic data
 We can use dictionary to pass the data from backend to template file
+We use model.py to define our class
+
+## For Static data
+we add the static folder in settings.py to access all the static files like css, js, bootstrap etc
+for adding static files we use {% static '${static_file}' %} in the main html template instead to directly giving href and src
 
 ## Word Counter
 Create a html in template to show the result, in views.py added the code to do processing, and routes added in urls.py
 
 ## Using POST method
-By using POST method we are making our website more secure as the strings passed by user is not shown in url, also by default in django csrf token is used which enhances the security
+By using POST method we are making our website more secure as the strings passed by user in forms is not shown in url, also by default in django csrf token is used which enhances the security
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,18 +1,63 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
-def index(request):
-    name = 'Rick'
-    context = {
-        'name':'Rick',
-        'age': 23,
-        'nationality':'Indian'
-    }
-    # return render(request, 'index.html', {'name':name})
-    return render(request, 'index.html', context)
+# def index(request):
+#     name = 'Rick'
+#     context = {
+#         'name':'Rick',
+#         'age': 23,
+#         'nationality':'Indian'
+#     }
+#     # return render(request, 'index.html', {'name':name})
+#     return render(request, 'index.html', context)
 
-def counter(request):
-    text = request.POST['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount':amount_of_words})
+# def counter(request):
+#     text = request.POST['text']
+#     amount_of_words = len(text.split())
+#     return render(request, 'counter.html', {'amount':amount_of_words})
+
+# Dynamic data test function
+
+def index(request):
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Basic Fitness'
+    feature1.details = 'Getting fit has now become very easy!'
+    feature1.is_true = True
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'New Gym Training'
+    feature2.details = 'Getting fit has now become very easy!'
+    feature2.is_true = False
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Basic Muscle Course'
+    feature3.details = 'Getting fit has now become very easy!'
+    feature3.is_true = False
+    
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Advanced Muscle Course'
+    feature4.details = 'Getting fit has now become very easy!'
+    feature4.is_true = False
+
+    feature5 = Feature()
+    feature5.id = 4
+    feature5.name = 'Yoga Training'
+    feature5.details = 'Getting fit has now become very easy!'
+    feature5.is_true = True
+
+    feature6 = Feature()
+    feature6.id = 5
+    feature6.name = 'Body Building Course'
+    feature6.details = 'Getting fit has now become very easy!'
+    feature6.is_true = False
+
+    featuresleft = [feature1, feature2, feature3]
+    featuresright = [feature4, feature5, feature6]
+
+    return render(request, 'index.html', {'featuresleft': featuresleft, 'featuresright': featuresright})
